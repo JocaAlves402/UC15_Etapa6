1d42cff83f4148e8a565d2437dbfb98eaf121ff5
JocaAlves402 <jocaalves.402@gmail.com>
2024-01-10 14:14:05 -0300

    Criação do API

---
+++ src/main/java/com/senac/UC15_Etapa7e8Atividade2/Uc15Etapa7e8Atividade2Application.java
@@ -0,0 +1,13 @@
+package com.senac.UC15_Etapa7e8Atividade2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Uc15Etapa7e8Atividade2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Uc15Etapa7e8Atividade2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/senac/UC15_Etapa7e8Atividade2/Uc15Etapa7e8Atividade2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senac.UC15_Etapa7e8Atividade2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Uc15Etapa7e8Atividade2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
